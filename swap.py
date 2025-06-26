ebe2cfe168389c9b051985d9923367da15eb7797
SinchanaMR-pixel <sinchanamr13@gmail.com>
2025-06-26 09:59:33 +0530

    Create swap.py

---
+++ swap.py
@@ -0,0 +1,16 @@
+# Swapping 5 and 6 using a temporary variable
+a = 5
+b = 6
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+
+# Swapping
+temp = a
+a = b
+b = temp
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
